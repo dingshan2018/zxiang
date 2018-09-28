cb4522993ef19dfd15a3a7dd9df34e20fe002475
wangjw <825755044@qq.com>
2018-09-28 10:27:12 +0800

    设备编号按照场所流水号3位自增

--- src/main/java/com/zxiang/project/business/device/service/DeviceServiceImpl.java
+++ src/main/java/com/zxiang/project/business/device/service/DeviceServiceImpl.java
@@ -8,6 +8,8 @@ import org.springframework.stereotype.Service;
 import com.zxiang.common.support.Convert;
 import com.zxiang.project.business.device.domain.Device;
 import com.zxiang.project.business.device.mapper.DeviceMapper;
+import com.zxiang.project.business.place.domain.Place;
+import com.zxiang.project.business.place.mapper.PlaceMapper;
 
 /**
  * 共享设备 服务层实现
@@ -20,7 +22,9 @@ public class DeviceServiceImpl implements IDeviceService
 {
 	@Autowired
 	private DeviceMapper deviceMapper;
-
+	@Autowired
+	private PlaceMapper placeMapper;
+	
 	/**
      * 查询共享设备信息
      * 
@@ -54,7 +58,37 @@ public class DeviceServiceImpl implements IDeviceService
 	@Override
 	public int insertDevice(Device device)
 	{
-	    return deviceMapper.insertDevice(device);
+		return getAutoCodeNum(device);
+	}
+	
+	/**
+	 * 设备编号为场所编号加3为序号
+	 * @param device
+	 * @return
+	 */
+	public synchronized int getAutoCodeNum(Device device){
+		//1.根据场所ID查询场所编号和地区编号
+		String placeId = device.getPlaceId();
+		Place placeEntity = placeMapper.selectPlaceById(Integer.parseInt(placeId));
+		String placeCode = placeEntity.getPlaceCode();
+
+		//2.根据地区编号查询该场所下最大设备编号
+		String currentMaxCode = deviceMapper.getMaxDeviceCode(placeId);
+		String deviceCode = "";
+		if(com.zxiang.common.utils.StringUtils.isNotEmpty(currentMaxCode)){
+			//设备编号为15位，场所编号为12位,若场所下存在设备编号则获取当前最大设备数量
+			String currentMax = currentMaxCode.substring(12);
+			int currentMaxNum = Integer.parseInt(currentMax);
+			++currentMaxNum;
+			deviceCode = String.format("%03d", currentMaxNum);
+		}else{
+			deviceCode = String.format("%03d", 1);
+		}
+		
+		//3.最大设备编号生成后插入新的流水设备编号
+		deviceCode = placeCode + deviceCode;
+		device.setDeviceCode(deviceCode);
+		return deviceMapper.insertDevice(device);
 	}
 	
 	/**
