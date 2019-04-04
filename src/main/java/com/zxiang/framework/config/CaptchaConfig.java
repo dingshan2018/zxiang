3ea97f45f1065a4be3f8d0363a943187d72beae7
linkd <397297021@qq.com>
2019-04-05 01:31:58 +0800

    验证码修改，定时器设置时间插件修改

--- src/main/java/com/zxiang/framework/config/CaptchaConfig.java
+++ src/main/java/com/zxiang/framework/config/CaptchaConfig.java
@@ -24,10 +24,10 @@ public class CaptchaConfig
         properties.setProperty("kaptcha.textproducer.font.color", "blue");
         properties.setProperty("kaptcha.image.width", "160");
         properties.setProperty("kaptcha.image.height", "60");
-        properties.setProperty("kaptcha.textproducer.font.size", "28");
+        properties.setProperty("kaptcha.textproducer.font.size", "48");
         properties.setProperty("kaptcha.session.key", "kaptchaCode");
         properties.setProperty("kaptcha.textproducer.char.spac", "35");
-        properties.setProperty("kaptcha.textproducer.char.length", "5");
+        properties.setProperty("kaptcha.textproducer.char.length", "4");
         properties.setProperty("kaptcha.textproducer.font.names", "Times New Roman");
         properties.setProperty("kaptcha.noise.color", "white");
         Config config = new Config(properties);
